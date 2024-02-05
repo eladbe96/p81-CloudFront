08d7ab67b115028d5c73359c512fcad6adbbe0de
Ubuntu <azureuser@Terraform-machine.gzvdlwwrvjpuzcxbgpmgw4w0ma.bx.internal.cloudapp.net>
2024-02-05 13:12:04 +0000

    changing the code

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import os
 
-s3_bucket_name_from_git = os.environ.get("S3_BUCKET_NAME")
+s3_bucket_name_from_git = os.environ.get("GITHUB_ENV")
 
 print(f'The bucket name is{s3_bucket_name_from_git}')
 
